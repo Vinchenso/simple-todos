8cdc8fccdee94149464b5ead8f25419d503869b4
Vinchenso Kendall <chensok@gmail.com>
2018-10-07 23:45:32 +0200

    add Collection

--- client/main.js
+++ client/main.js
@@ -1,22 +1,17 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
 
-import './main.html';
+import { Meteor } from 'meteor/meteor';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import { render } from 'react-dom';
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+ 
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+import App from '../imports/ui/App.js';
+
+ 
+
+Meteor.startup(() => {
+
+  render(<App />, document.getElementById('render-target'));
+
 });
